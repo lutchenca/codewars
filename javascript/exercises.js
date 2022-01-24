d98d274b13208d5d2cdbfa4c362f68d67d4cf712
Lut <lutchenca@hotmail.com>
2022-01-24 10:35:54 -0300

    reverse a word exercise

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -1,3 +1,4 @@
+// 01   
 // Convert number to reversed array of digits
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 
@@ -5,5 +6,16 @@ function digitize(num) {
     return num.toString().split('').reverse().map(Number);
 }
 
-// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+
+// 02 
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+function solution(str){
+    let splitStr = str.split('');
+    let reverseStr = splitStr.reverse();
+    let joinStr = reverseStr.join('');
+    return joinStr;
+}
 
---
+++ javascript/saved-links.md
@@ -0,0 +1,2 @@
+02 - reverse strings
+freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
