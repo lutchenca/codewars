c9589a210554b9a5324511e3b9da88710f3abd04
Lut <lutchenca@hotmail.com>
2022-01-25 13:21:09 -0300

    missing exercise 04

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Exercises provide by [CodeWars](https://www.codewars.com/).
 Codewars is a platform that helps you learn, train, and improve your coding skills by solving programming tasks of many types and difficulty levels.
+
+8kuy - easy ones
---
+++ javascript/8kyu.js
@@ -0,0 +1,38 @@
+// 01   
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+function digitize(num) {
+    return num.toString().split('').reverse().map(Number);
+}
+
+
+
+// 02 
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+function solution(str){
+    let splitStr = str.split('');
+    let reverseStr = splitStr.reverse();
+    let joinStr = reverseStr.join('');
+    return joinStr;
+}
+
+// 03
+// Write a function to split a string and convert it into an array of words. For example:
+// "Robin Singh" ==> ["Robin", "Singh"]
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string){
+    let stringToArray = string.split(" ");
+    return stringToArray;
+  }
+
+// 04
+// Complete the solution so that it reverses all of the words within the string passed in.
+// Example:
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+function reverseWords(str){
+    return str.reverse; 
+  }
